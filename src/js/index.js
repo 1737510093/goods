2bd6077252e8fe92f3367ebe8265f07afecb0184
1737510093 <1737510093@qq.com>
2018-06-27 14:57:02 +0800

    更新项目

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
 require(["config"],function(){
-	require(["load"],function(){
-		
-	})
+	require(["load"],function(){});
+	require(["jquery"],function(){});
+	require(["unslider"],function(){});
+	
+	
 })
+//主页轮播图
+$(function(){
+		$(document).ready(function(e) {
+		    var unslider04 = $('#b04').unslider({
+		        dots: true
+			    }),
+			    data04 = unslider04.data('unslider');
+			     
+			    $('.unslider-arrow04').click(function() {
+			        var fn = this.className.split(' ')[1];
+			        data04[fn]();
+			    });
+		});
+		
+		//侧边栏影藏元素动态展示
+		$(".slide_nav").children("div").hover(function(){
+			$(this).children("div").slideToggle("fast");
+		})
+})
+		
+
+
+
--- src/js/loadHeaderAndFooter.js
+++ src/js/loadHeaderAndFooter.js
@@ -2,4 +2,30 @@
 define(["jquery"],function($){
 	$(".header").load("/html/include/header.html");
 	$(".footer").load("/html/include/footer.html");
+	//搜索框聚焦
+	$(function(){
+		//搜索关键字
+		$(".index_search").on("keyup",function(){
+			const txt =$(this).val();
+			const url = `https://suggest.taobao.com/sug?code=utf-8&q=${txt}&callback=?`;
+			$.getJSON(url,function(data){
+				let html ="";
+				data.result.forEach(function(curr){
+					html+=`<div>${curr[0]}</div>`
+				})
+				$(".show_search").show().html(html);
+			});
+		});
+		//使用事件委派点击关键词更新到搜索框
+		$(".show_search").delegate("div","click",function(){
+			const txt = $(this).text();
+			$(".index_search").val(txt);
+			$(".show_search").hide();
+		})
+		
+		//导航栏下拉列表
+		$(".allMenu li").hover(function(){
+			$(this).find($(".menu_list")).slideToggle("fast");
+		})
+	})
 });
